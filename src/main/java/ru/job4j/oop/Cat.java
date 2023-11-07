f477e937d30dad7dd434daddcab9b8083944f7c2
nastya <gleddders@gmail.com>
2023-11-07 21:29:30 +0400

    Поля объекта [#166083 #328301]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -3,13 +3,23 @@ package ru.job4j.oop;
 public class Cat {
     private String food;
 
+    private String name;
+
     private static int number;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
 
     public String sound() {
         return "may-may";
     }
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public void show() {
-        System.out.println(this.food);
+        System.out.println(this.name + ' ' + this.food);
     }
 
     public static void main(String[] args) {
@@ -18,7 +28,10 @@ public class Cat {
         gav.show();
         System.out.println("There is black's food.");
         Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("kotleta");
         black.show();
+
         System.out.println(number);
     }
 }
